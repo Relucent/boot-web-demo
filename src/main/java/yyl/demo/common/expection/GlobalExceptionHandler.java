fc661eee516422dbc91626803d002d9ab3896300
yyl <relucent@163.com>
2019-09-05 12:08:59 +0800

    -+

---
+++ src/main/java/yyl/demo/common/expection/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package yyl.demo.common.expection;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.github.relucent.base.util.expection.ErrorType;
+import com.github.relucent.base.util.expection.GeneralException;
+import com.github.relucent.base.util.model.Result;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    protected Result<?> handleGlobalException(Exception exception) {
+        log(exception);
+        String message = exception.getMessage();
+        if (message == null) {
+            message = "Service Error !";
+        }
+        return Result.error(message);
+    }
+
+    protected void log(Exception e) {
+        if (e instanceof GeneralException && ErrorType.PROMPT.equals(((GeneralException) e).getType())) {
+            log.warn(e.getMessage());
+        } else if (e instanceof org.apache.catalina.connector.ClientAbortException) {
+            log.warn(e.toString());
+        } else {
+            log.error("!", e);
+        }
+    }
+}
