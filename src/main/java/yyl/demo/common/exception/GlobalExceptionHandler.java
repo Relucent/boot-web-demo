dd0d205c478c5bab6e8b798d22586371a747be7e
yyl <relucent@163.com>
2020-02-19 10:14:43 +0800

    ^

--- src/main/java/yyl/demo/common/exception/GlobalExceptionHandler.java
+++ src/main/java/yyl/demo/common/exception/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package yyl.demo.common.exception;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.github.relucent.base.common.exception.ErrorType;
 import com.github.relucent.base.common.exception.GeneralException;
 import com.github.relucent.base.plugin.model.Result;
@@ -14,24 +17,44 @@ import lombok.extern.slf4j.Slf4j;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    @ResponseBody
-    protected Result<?> handleGlobalException(Exception exception) {
-        log(exception);
-        String message = exception.getMessage();
-        if (message == null) {
-            message = "Service Error !";
-        }
-        return Result.error(message);
-    }
+	@ExceptionHandler(Exception.class)
+	@ResponseBody
+	protected Result<?> handleGlobalException(Exception exception) {
+		log(exception);
+		final String message;
+		if (exception instanceof HttpMessageNotReadableException) {
+			Throwable cause = exception.getCause();
+			if (cause instanceof JsonParseException) {
+				message = "Request Json Format Error!";
+			} else {
+				message = "Request Format Error!";
+			}
+		} else if (isDbException(exception)) {
+			message = "Data Exception!";
+		} else {
+			message = "Service Error!";
+		}
+		return Result.error(message);
+	}
 
-    protected void log(Exception e) {
-        if (e instanceof GeneralException && ErrorType.PROMPT.equals(((GeneralException) e).getType())) {
-            log.warn(e.getMessage());
-        } else if (e instanceof org.apache.catalina.connector.ClientAbortException) {
-            log.warn(e.toString());
-        } else {
-            log.error("!", e);
-        }
-    }
+	protected void log(Exception e) {
+		if (e instanceof GeneralException && ErrorType.PROMPT.equals(((GeneralException) e).getType())) {
+			log.warn(e.getMessage());
+		} else if (e instanceof org.apache.catalina.connector.ClientAbortException) {
+			log.warn(e.toString());
+		} else {
+			log.error("!", e);
+		}
+	}
+
+	private static boolean isDbException(Exception e) {
+		Throwable error = (Throwable) e;
+		for (int i = 0; i < 100 && error != null; i++) {
+			if (error instanceof DataAccessException) {
+				return true;
+			}
+			error = error.getCause();
+		}
+		return false;
+	}
 }
