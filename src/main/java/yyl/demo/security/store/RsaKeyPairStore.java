60bbfb236f02e96f8c354399ba4f1ce32c570c2d
yyl <relucent@163.com>
2022-06-29 15:40:08 +0800

    √

--- src/main/java/yyl/demo/security/store/RsaKeyPairStore.java
+++ src/main/java/yyl/demo/security/store/RsaKeyPairStore.java
@@ -10,12 +10,12 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.StringUtils;
 
+import com.github.relucent.base.common.identifier.IdUtil;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import yyl.demo.common.util.IdUtil;
 import yyl.demo.common.util.RsaUtil;
 
 /**
@@ -24,74 +24,74 @@ import yyl.demo.common.util.RsaUtil;
 @Slf4j
 public class RsaKeyPairStore {
 
-    // ==============================Fields===========================================
-    private final Cache<String, KeyPairElement> cache;
+	// ==============================Fields===========================================
+	private final Cache<String, KeyPairElement> cache;
 
-    // ==============================Constructors=====================================
-    /**
-     * 构造函数
-     */
-    public RsaKeyPairStore() {
-        Duration ttl = Duration.parse("PT30S");
-        cache = CacheBuilder.newBuilder()//
-                .expireAfterWrite(ttl.toMillis(), TimeUnit.MILLISECONDS)//
-                .expireAfterAccess(ttl.toMillis(), TimeUnit.MILLISECONDS)//
-                .build();
-    }
+	// ==============================Constructors=====================================
+	/**
+	 * 构造函数
+	 */
+	public RsaKeyPairStore() {
+		Duration ttl = Duration.parse("PT30S");
+		cache = CacheBuilder.newBuilder()//
+				.expireAfterWrite(ttl.toMillis(), TimeUnit.MILLISECONDS)//
+				.expireAfterAccess(ttl.toMillis(), TimeUnit.MILLISECONDS)//
+				.build();
+	}
 
-    // ==============================Methods==========================================
-    /**
-     * 存储密钥对
-     * @param keyPair 密钥对
-     * @return 存储ID
-     */
-    public String put(KeyPair keyPair) {
-        String id = IdUtil.uuid32();
-        PublicKey publicKey = keyPair.getPublic();
-        PrivateKey privateKey = keyPair.getPrivate();
-        KeyPairElement element = new KeyPairElement();
-        element.setPublicKey(RsaUtil.encodeBase64String(publicKey));
-        element.setPrivateKey(RsaUtil.encodeBase64String(privateKey));
-        cache.put(id, element);
-        return id;
-    }
+	// ==============================Methods==========================================
+	/**
+	 * 存储密钥对
+	 * @param keyPair 密钥对
+	 * @return 存储ID
+	 */
+	public String put(KeyPair keyPair) {
+		String id = IdUtil.uuid32();
+		PublicKey publicKey = keyPair.getPublic();
+		PrivateKey privateKey = keyPair.getPrivate();
+		KeyPairElement element = new KeyPairElement();
+		element.setPublicKey(RsaUtil.encodeBase64String(publicKey));
+		element.setPrivateKey(RsaUtil.encodeBase64String(privateKey));
+		cache.put(id, element);
+		return id;
+	}
 
-    /**
-     * 根据ID获取密钥对
-     * @param id 密钥对ID
-     * @return 密钥对
-     */
-    public KeyPair get(String id) {
-        if (StringUtils.isEmpty(id)) {
-            return null;
-        }
-        KeyPairElement element = cache.getIfPresent(id);
-        if (element == null) {
-            return null;
-        }
-        try {
-            PublicKey publicKey = RsaUtil.decodeBase64PublicKey(element.getPublicKey());
-            PrivateKey privateKey = RsaUtil.decodeBase64PrivateKey(element.getPrivateKey());
-            return new KeyPair(publicKey, privateKey);
-        } catch (Exception e) {
-            log.error("!", e);
-        }
-        return null;
-    }
+	/**
+	 * 根据ID获取密钥对
+	 * @param id 密钥对ID
+	 * @return 密钥对
+	 */
+	public KeyPair get(String id) {
+		if (StringUtils.isEmpty(id)) {
+			return null;
+		}
+		KeyPairElement element = cache.getIfPresent(id);
+		if (element == null) {
+			return null;
+		}
+		try {
+			PublicKey publicKey = RsaUtil.decodeBase64PublicKey(element.getPublicKey());
+			PrivateKey privateKey = RsaUtil.decodeBase64PrivateKey(element.getPrivateKey());
+			return new KeyPair(publicKey, privateKey);
+		} catch (Exception e) {
+			log.error("!", e);
+		}
+		return null;
+	}
 
-    /**
-     * 删除密钥对
-     * @param id 密钥对ID
-     */
-    public void remove(String id) {
-        Optional.ofNullable(id).ifPresent(cache::invalidate);
-    }
+	/**
+	 * 删除密钥对
+	 * @param id 密钥对ID
+	 */
+	public void remove(String id) {
+		Optional.ofNullable(id).ifPresent(cache::invalidate);
+	}
 
-    // ==============================InnerClass=======================================
-    @SuppressWarnings("serial")
-    @Data
-    public static class KeyPairElement implements Serializable {
-        private String privateKey;
-        private String publicKey;
-    }
+	// ==============================InnerClass=======================================
+	@SuppressWarnings("serial")
+	@Data
+	public static class KeyPairElement implements Serializable {
+		private String privateKey;
+		private String publicKey;
+	}
 }
--- src/main/java/yyl/demo/service/UserService.java
+++ src/main/java/yyl/demo/service/UserService.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.github.relucent.base.common.exception.ExceptionHelper;
+import com.github.relucent.base.common.identifier.IdUtil;
 
 import yyl.demo.common.constant.IdConstant;
 import yyl.demo.common.constant.SymbolConstant;
@@ -24,7 +25,6 @@ import yyl.demo.common.model.PageVO;
 import yyl.demo.common.model.PaginationQO;
 import yyl.demo.common.mybatis.MyPageHelper;
 import yyl.demo.common.standard.AuditableUtil;
-import yyl.demo.common.util.IdUtil;
 import yyl.demo.common.util.RsaUtil;
 import yyl.demo.entity.UserEntity;
 import yyl.demo.entity.UserRoleEntity;
@@ -47,270 +47,270 @@ import yyl.demo.security.store.RsaKeyPairStore;
 @Service
 public class UserService {
 
-    // ==============================Fields===========================================
-    @Autowired
-    private UserMapper userMapper;
-    @Autowired
-    private UserRoleMapper userRoleMapper;
-    @Autowired
-    private OrganizationMapper organizationMapper;
+	// ==============================Fields===========================================
+	@Autowired
+	private UserMapper userMapper;
+	@Autowired
+	private UserRoleMapper userRoleMapper;
+	@Autowired
+	private OrganizationMapper organizationMapper;
 
-    @Autowired
-    private RsaKeyPairStore rsaKeyPairStore;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+	@Autowired
+	private RsaKeyPairStore rsaKeyPairStore;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
-    // ==============================Methods==========================================
-    /**
-     * 新增用户
-     * @param dto 用户信息
-     */
-    public void save(UserDTO dto) {
+	// ==============================Methods==========================================
+	/**
+	 * 新增用户
+	 * @param dto 用户信息
+	 */
+	public void save(UserDTO dto) {
 
-        dto.setId(null);
-        validate(dto);
+		dto.setId(null);
+		validate(dto);
 
-        UserEntity entity = new UserEntity();
-        UserKit.copyProperties(dto, entity);
+		UserEntity entity = new UserEntity();
+		UserKit.copyProperties(dto, entity);
 
-        entity.setId(IdUtil.uuid32());
+		entity.setId(IdUtil.uuid32());
 
-        // 初始密码和用户名一致
-        entity.setPassword(passwordEncoder.encode(entity.getUsername()));
+		// 初始密码和用户名一致
+		entity.setPassword(passwordEncoder.encode(entity.getUsername()));
 
-        if (entity.getEnabled() == null) {
-            entity.setEnabled(IntBoolEnum.N.value());
-        }
+		if (entity.getEnabled() == null) {
+			entity.setEnabled(IntBoolEnum.N.value());
+		}
 
-        String identity = Securitys.getUserId();
-        AuditableUtil.setCreated(entity, identity);
-        userMapper.insert(entity);
+		String identity = Securitys.getUserId();
+		AuditableUtil.setCreated(entity, identity);
+		userMapper.insert(entity);
 
-        // 更新用户角色
-        updateUserRole(entity.getId(), dto.getRoleIds());
-    }
+		// 更新用户角色
+		updateUserRole(entity.getId(), dto.getRoleIds());
+	}
 
-    /**
-     * 删除用户(标记删除)
-     * @param id 用户ID
-     */
-    public void deleteById(String id) {
-        if (IdConstant.ADMIN_ID.equals(id)) {
-            throw ExceptionHelper.prompt("系统管理员不能被删除");
-        }
-        userRoleMapper.deleteByUserId(id);
+	/**
+	 * 删除用户(标记删除)
+	 * @param id 用户ID
+	 */
+	public void deleteById(String id) {
+		if (IdConstant.ADMIN_ID.equals(id)) {
+			throw ExceptionHelper.prompt("系统管理员不能被删除");
+		}
+		userRoleMapper.deleteByUserId(id);
 
-        // 删除用户数据(标记删除)
-        UserEntity updated = new UserEntity();
-        updated.setId(id);
-        updated.setDeleted(IntBoolEnum.Y.value());
-        String identity = Securitys.getUserId();
-        AuditableUtil.setUpdated(updated, identity);
-        userMapper.updateById(updated);
-    }
+		// 删除用户数据(标记删除)
+		UserEntity updated = new UserEntity();
+		updated.setId(id);
+		updated.setDeleted(IntBoolEnum.Y.value());
+		String identity = Securitys.getUserId();
+		AuditableUtil.setUpdated(updated, identity);
+		userMapper.updateById(updated);
+	}
 
-    /**
-     * 更新用户
-     * @param dto 用户信息
-     */
-    public void update(UserDTO dto) {
-        validate(dto);
+	/**
+	 * 更新用户
+	 * @param dto 用户信息
+	 */
+	public void update(UserDTO dto) {
+		validate(dto);
 
-        UserEntity entity = userMapper.selectById(dto.getId());
-        if (entity == null) {
-            throw ExceptionHelper.prompt("用户不存在或者已经失效");
-        }
-        UserKit.copyProperties(dto, entity);
+		UserEntity entity = userMapper.selectById(dto.getId());
+		if (entity == null) {
+			throw ExceptionHelper.prompt("用户不存在或者已经失效");
+		}
+		UserKit.copyProperties(dto, entity);
 
-        // 不修改密码
-        entity.setPassword(null);
+		// 不修改密码
+		entity.setPassword(null);
 
-        String identity = Securitys.getUserId();
-        AuditableUtil.setUpdated(entity, identity);
-        userMapper.updateById(entity);
+		String identity = Securitys.getUserId();
+		AuditableUtil.setUpdated(entity, identity);
+		userMapper.updateById(entity);
 
-        // 更新用户角色
-        updateUserRole(entity.getId(), dto.getRoleIds());
-    }
+		// 更新用户角色
+		updateUserRole(entity.getId(), dto.getRoleIds());
+	}
 
-    /**
-     * 用户启用禁用
-     * @param id 用户ID
-     * @param enabled 是否启用
-     */
-    public void enableById(String id, Integer enabled) {
-        if (IdConstant.ADMIN_ID.equals(id)) {
-            throw ExceptionHelper.prompt("内置账号不能被禁用");
-        }
-        IntBoolEnum bool = IntBoolEnum.of(enabled, null);
-        if (bool != null && StringUtils.isNoneEmpty(id)) {
-            UserEntity updated = new UserEntity();
-            updated.setId(id);
-            updated.setEnabled(bool.value());
-            String identity = Securitys.getUserId();
-            AuditableUtil.setCreated(updated, identity);
-            userMapper.updateById(updated);
-        }
-    }
+	/**
+	 * 用户启用禁用
+	 * @param id 用户ID
+	 * @param enabled 是否启用
+	 */
+	public void enableById(String id, Integer enabled) {
+		if (IdConstant.ADMIN_ID.equals(id)) {
+			throw ExceptionHelper.prompt("内置账号不能被禁用");
+		}
+		IntBoolEnum bool = IntBoolEnum.of(enabled, null);
+		if (bool != null && StringUtils.isNoneEmpty(id)) {
+			UserEntity updated = new UserEntity();
+			updated.setId(id);
+			updated.setEnabled(bool.value());
+			String identity = Securitys.getUserId();
+			AuditableUtil.setCreated(updated, identity);
+			userMapper.updateById(updated);
+		}
+	}
 
-    /**
-     * 重置用户密码
-     * @param id 用户ID
-     */
-    public void resetPasswordById(String id) {
-        String identity = Securitys.getUserId();
-        UserEntity entity = userMapper.selectById(id);
-        if (entity == null) {
-            throw ExceptionHelper.prompt("用户不存在或者已经失效");
-        }
-        String password = entity.getUsername();
-        entity.setPassword(passwordEncoder.encode(password));
-        AuditableUtil.setUpdated(entity, identity);
-        userMapper.updateById(entity);
-    }
+	/**
+	 * 重置用户密码
+	 * @param id 用户ID
+	 */
+	public void resetPasswordById(String id) {
+		String identity = Securitys.getUserId();
+		UserEntity entity = userMapper.selectById(id);
+		if (entity == null) {
+			throw ExceptionHelper.prompt("用户不存在或者已经失效");
+		}
+		String password = entity.getUsername();
+		entity.setPassword(passwordEncoder.encode(password));
+		AuditableUtil.setUpdated(entity, identity);
+		userMapper.updateById(entity);
+	}
 
-    /**
-     * 修改当前用户密码
-     * @param passwordDto 新旧密码
-     */
-    public void updatePasswordByCurrent(PasswordDTO passwordDto) {
+	/**
+	 * 修改当前用户密码
+	 * @param passwordDto 新旧密码
+	 */
+	public void updatePasswordByCurrent(PasswordDTO passwordDto) {
 
-        String identity = Securitys.getUserId();
-        String oldPassword = passwordDto.getOldPassword();
-        String newPassword = passwordDto.getNewPassword();
-        String rsaId = passwordDto.getRsaId();
+		String identity = Securitys.getUserId();
+		String oldPassword = passwordDto.getOldPassword();
+		String newPassword = passwordDto.getNewPassword();
+		String rsaId = passwordDto.getRsaId();
 
-        if (StringUtils.isEmpty(oldPassword)) {
-            throw ExceptionHelper.prompt("请输入旧密码");
-        }
-        if (StringUtils.isEmpty(newPassword)) {
-            throw ExceptionHelper.prompt("请输入新密码");
-        }
+		if (StringUtils.isEmpty(oldPassword)) {
+			throw ExceptionHelper.prompt("请输入旧密码");
+		}
+		if (StringUtils.isEmpty(newPassword)) {
+			throw ExceptionHelper.prompt("请输入新密码");
+		}
 
-        // 使用RSA加密
-        if (StringUtils.isNotEmpty(rsaId)) {
-            KeyPair keyPair = rsaKeyPairStore.get(rsaId);
-            if (keyPair == null) {
-                throw ExceptionHelper.prompt("客户端秘钥失效！");
-            }
-            try {
-                PrivateKey privateKey = keyPair.getPrivate();
-                oldPassword = RsaUtil.decryptBase64(oldPassword, privateKey);
-                newPassword = RsaUtil.decryptBase64(newPassword, privateKey);
-                rsaKeyPairStore.remove(rsaId);
-            } catch (Exception e) {
-                throw ExceptionHelper.prompt("客户端秘钥无效!");
-            }
-        }
+		// 使用RSA加密
+		if (StringUtils.isNotEmpty(rsaId)) {
+			KeyPair keyPair = rsaKeyPairStore.get(rsaId);
+			if (keyPair == null) {
+				throw ExceptionHelper.prompt("客户端秘钥失效！");
+			}
+			try {
+				PrivateKey privateKey = keyPair.getPrivate();
+				oldPassword = RsaUtil.decryptBase64(oldPassword, privateKey);
+				newPassword = RsaUtil.decryptBase64(newPassword, privateKey);
+				rsaKeyPairStore.remove(rsaId);
+			} catch (Exception e) {
+				throw ExceptionHelper.prompt("客户端秘钥无效!");
+			}
+		}
 
-        String userId = identity;
-        UserEntity entity = userMapper.selectById(userId);
-        if (entity == null) {
-            throw ExceptionHelper.prompt("用户未登录");
-        }
-        if (!passwordEncoder.matches(oldPassword, entity.getPassword())) {
-            throw ExceptionHelper.prompt("旧密码输入错误");
-        }
-        entity.setPassword(passwordEncoder.encode(newPassword));
-        AuditableUtil.setUpdated(entity, identity);
-        userMapper.updateById(entity);
-    }
+		String userId = identity;
+		UserEntity entity = userMapper.selectById(userId);
+		if (entity == null) {
+			throw ExceptionHelper.prompt("用户未登录");
+		}
+		if (!passwordEncoder.matches(oldPassword, entity.getPassword())) {
+			throw ExceptionHelper.prompt("旧密码输入错误");
+		}
+		entity.setPassword(passwordEncoder.encode(newPassword));
+		AuditableUtil.setUpdated(entity, identity);
+		userMapper.updateById(entity);
+	}
 
-    /**
-     * 查询用户
-     * @param id 用户ID
-     * @return 用户信息
-     */
-    public UserVO getById(String id) {
-        UserEntity entity = userMapper.selectById(id);
-        if (entity != null) {
-            entity.setPassword(SymbolConstant.PASSWORD_PLACEHOLDER);
-        }
-        UserVO vo = UserKit.toVO(entity);
-        vo.setOrganizationName(organizationMapper.getNameById(entity.getOrganizationId()));
+	/**
+	 * 查询用户
+	 * @param id 用户ID
+	 * @return 用户信息
+	 */
+	public UserVO getById(String id) {
+		UserEntity entity = userMapper.selectById(id);
+		if (entity != null) {
+			entity.setPassword(SymbolConstant.PASSWORD_PLACEHOLDER);
+		}
+		UserVO vo = UserKit.toVO(entity);
+		vo.setOrganizationName(organizationMapper.getNameById(entity.getOrganizationId()));
 
-        List<String> roleIds = userRoleMapper.findRoleIdListByUserId(id);
-        vo.setRoleIds(roleIds);
-        return vo;
-    }
+		List<String> roleIds = userRoleMapper.findRoleIdListByUserId(id);
+		vo.setRoleIds(roleIds);
+		return vo;
+	}
 
-    /**
-     * 查询用户
-     * @param username 用户名
-     * @return 用户信息
-     */
-    public UserEntity getByUsername(String username) {
-        return userMapper.getByUsername(username);
-    }
+	/**
+	 * 查询用户
+	 * @param username 用户名
+	 * @return 用户信息
+	 */
+	public UserEntity getByUsername(String username) {
+		return userMapper.getByUsername(username);
+	}
 
-    /**
-     * 分页查询
-     * @param pagination 分页条件
-     * @return 分页结果
-     */
-    public PageVO<UserRO> list(PaginationQO<UserQO> pagination) {
-        UserQO qo = pagination.getFilter();
-        PageVO<UserEntity> page = MyPageHelper.invoke(pagination, () -> userMapper.findByCriteria(qo));
-        return page.mapRecords(UserKit::toRO);
-    }
+	/**
+	 * 分页查询
+	 * @param pagination 分页条件
+	 * @return 分页结果
+	 */
+	public PageVO<UserRO> list(PaginationQO<UserQO> pagination) {
+		UserQO qo = pagination.getFilter();
+		PageVO<UserEntity> page = MyPageHelper.invoke(pagination, () -> userMapper.findByCriteria(qo));
+		return page.mapRecords(UserKit::toRO);
+	}
 
-    /**
-     * 更新用户的角色
-     * @param userId 用户ID
-     * @param roleIds 角色 ID
-     */
-    private void updateUserRole(String userId, Collection<String> roleIds) {
-        if (roleIds == null) {
-            return;
-        }
-        String identity = Securitys.getUserId();
-        Map<String, UserRoleEntity> roleIdMap = new HashMap<>();
-        for (UserRoleEntity entity : userRoleMapper.findByUserId(userId)) {
-            roleIdMap.put(entity.getRoleId(), entity);
-        }
-        List<UserRoleEntity> newEntities = new ArrayList<>();
-        for (String roleId : roleIds) {
-            UserRoleEntity entity = roleIdMap.remove(roleId);
-            if (entity == null) {
-                entity = new UserRoleEntity();
-                entity.setId(IdUtil.uuid32());
-                entity.setUserId(userId);
-                entity.setRoleId(roleId);
-                AuditableUtil.setCreated(entity, identity);
-                newEntities.add(entity);
-            }
-        }
-        for (UserRoleEntity entity : roleIdMap.values()) {
-            userRoleMapper.deleteById(entity.getId());
-        }
-        for (UserRoleEntity entity : newEntities) {
-            userRoleMapper.insert(entity);
-        }
-    }
+	/**
+	 * 更新用户的角色
+	 * @param userId 用户ID
+	 * @param roleIds 角色 ID
+	 */
+	private void updateUserRole(String userId, Collection<String> roleIds) {
+		if (roleIds == null) {
+			return;
+		}
+		String identity = Securitys.getUserId();
+		Map<String, UserRoleEntity> roleIdMap = new HashMap<>();
+		for (UserRoleEntity entity : userRoleMapper.findByUserId(userId)) {
+			roleIdMap.put(entity.getRoleId(), entity);
+		}
+		List<UserRoleEntity> newEntities = new ArrayList<>();
+		for (String roleId : roleIds) {
+			UserRoleEntity entity = roleIdMap.remove(roleId);
+			if (entity == null) {
+				entity = new UserRoleEntity();
+				entity.setId(IdUtil.uuid32());
+				entity.setUserId(userId);
+				entity.setRoleId(roleId);
+				AuditableUtil.setCreated(entity, identity);
+				newEntities.add(entity);
+			}
+		}
+		for (UserRoleEntity entity : roleIdMap.values()) {
+			userRoleMapper.deleteById(entity.getId());
+		}
+		for (UserRoleEntity entity : newEntities) {
+			userRoleMapper.insert(entity);
+		}
+	}
 
-    // ==============================ToolMethods======================================
-    /**
-     * 验证数据
-     * @param dto 用户数据
-     */
-    private void validate(UserDTO dto) {
-        String id = dto.getId();
-        String username = dto.getUsername();
-        String realname = dto.getRealname();
-        String organizationId = dto.getOrganizationId();
+	// ==============================ToolMethods======================================
+	/**
+	 * 验证数据
+	 * @param dto 用户数据
+	 */
+	private void validate(UserDTO dto) {
+		String id = dto.getId();
+		String username = dto.getUsername();
+		String realname = dto.getRealname();
+		String organizationId = dto.getOrganizationId();
 
-        if (StringUtils.isEmpty(username)) {
-            throw ExceptionHelper.prompt("用户名不能为空");
-        }
-        if (StringUtils.isEmpty(realname)) {
-            throw ExceptionHelper.prompt("姓名不能为空");
-        }
-        UserEntity entity = userMapper.getByUsername(username);
-        if (entity != null && !Objects.equals(entity.getId(), id)) {
-            throw ExceptionHelper.prompt("已经存在相同账号");
-        }
-        if (IdConstant.ADMIN_ID.equals(id) && organizationMapper.getById(organizationId) == null) {
-            throw ExceptionHelper.prompt("该组织机构无效");
-        }
-    }
+		if (StringUtils.isEmpty(username)) {
+			throw ExceptionHelper.prompt("用户名不能为空");
+		}
+		if (StringUtils.isEmpty(realname)) {
+			throw ExceptionHelper.prompt("姓名不能为空");
+		}
+		UserEntity entity = userMapper.getByUsername(username);
+		if (entity != null && !Objects.equals(entity.getId(), id)) {
+			throw ExceptionHelper.prompt("已经存在相同账号");
+		}
+		if (IdConstant.ADMIN_ID.equals(id) && organizationMapper.getById(organizationId) == null) {
+			throw ExceptionHelper.prompt("该组织机构无效");
+		}
+	}
 }
