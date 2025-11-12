a82a7be101370f7e25ccdb9bd9dcb3ee777a8fdf
yyl <relucent@163.com>
2025-11-12 10:25:56 +0800

    #

--- src/main/java/yyl/demo/controller/AuthController.java
+++ src/main/java/yyl/demo/controller/AuthController.java
@@ -11,10 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.relucent.base.plugin.model.Result;
-import com.github.relucent.base.plugin.spring.context.WebContextHolder;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import yyl.demo.common.context.WebContextHolder;
 import yyl.demo.model.dto.UsernamePasswordDTO;
 import yyl.demo.model.vo.RsaPublicKeyVO;
 import yyl.demo.model.vo.UserInfoVO;
@@ -26,41 +26,41 @@ import yyl.demo.service.AuthenticationTokenService;
 @RequestMapping("/rest/auth")
 public class AuthController {
 
-	// ==============================Fields===========================================
-	@Autowired
-	private AuthenticationTokenService authenticationTokenService;
+    // ==============================Fields===========================================
+    @Autowired
+    private AuthenticationTokenService authenticationTokenService;
 
-	// ==============================Methods==========================================
-	@Operation(summary = "获取加密公钥")
-	@PostMapping("/getPublicKey")
-	@PermitAll
-	public Result<RsaPublicKeyVO> getPublicKey() {
-		RsaPublicKeyVO vo = authenticationTokenService.getPublicKey();
-		return Result.ok(vo);
-	}
+    // ==============================Methods==========================================
+    @Operation(summary = "获取加密公钥")
+    @PostMapping("/getPublicKey")
+    @PermitAll
+    public Result<RsaPublicKeyVO> getPublicKey() {
+        RsaPublicKeyVO vo = authenticationTokenService.getPublicKey();
+        return Result.ok(vo);
+    }
 
-	@Operation(summary = "用户登录")
-	@PostMapping("/login")
-	@PermitAll
-	public Result<AccessToken> doLogin(@RequestBody UsernamePasswordDTO dto) {
-		AccessToken token = authenticationTokenService.login(dto);
-		return Result.ok(token);
-	}
+    @Operation(summary = "用户登录")
+    @PostMapping("/login")
+    @PermitAll
+    public Result<AccessToken> doLogin(@RequestBody UsernamePasswordDTO dto) {
+        AccessToken token = authenticationTokenService.login(dto);
+        return Result.ok(token);
+    }
 
-	@Operation(summary = " 用户登出")
-	@GetMapping("/logout")
-	@PostMapping("/logout")
-	@PermitAll
-	public Result<?> logout() {
-		authenticationTokenService.logout();
-		WebContextHolder.invalidateSession();
-		return Result.ok();
-	}
+    @Operation(summary = " 用户登出")
+    @GetMapping("/logout")
+    @PostMapping("/logout")
+    @PermitAll
+    public Result<?> logout() {
+        authenticationTokenService.logout();
+        WebContextHolder.invalidateSession();
+        return Result.ok();
+    }
 
-	@Operation(summary = "用户信息")
-	@PostMapping("/info")
-	public Result<UserInfoVO> info(HttpServletRequest request) {
-		UserInfoVO info = authenticationTokenService.getCurrentUser();
-		return Result.ok(info);
-	}
+    @Operation(summary = "用户信息")
+    @PostMapping("/info")
+    public Result<UserInfoVO> info(HttpServletRequest request) {
+        UserInfoVO info = authenticationTokenService.getCurrentUser();
+        return Result.ok(info);
+    }
 }
